433cba1397d0937411d6070db2594f7bcd5b59e7
shizhilvren <shizhilvren@foxmail.com>
2025-02-03 10:53:01 -0500

    try to package

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder};
 fn main() -> Result<()> {
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
-        .process_current_dir()
+        .process()
         .unwrap();
     let build = BuildBuilder::all_build()?;
     let gix = GixBuilder::all_git()?;
